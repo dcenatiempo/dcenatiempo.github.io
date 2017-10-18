7af0e5183874c7b9ab81c1b4a658d4072f2c3b6b
Devin Cenatiempo <dcenatiempo@gmail.com>
2017-10-18 13:53:58 -0600

    changed fonts, logo, removed contact form

--- js/main.js
+++ js/main.js
@@ -1,30 +1,44 @@
-function myFunction() {
-  $("#nav").toggleClass("change");
-  $("#small-nav").toggleClass("hidden");
-  //console.log($("#small-nav").attr("class"));
-};
+$(document).ready( () => {
+  function expandHamburgerMenu() {
+    $("#nav").addClass("change");
+    $("#small-nav").removeClass("hidden");
+  };
+  function collapseHamburgerMenu() {
+    $("#nav").removeClass("change");
+    $("#small-nav").addClass("hidden");
+  };
+  function toggleHamburgerMenu() {
+    $("#nav").toggleClass("change");
+    $("#small-nav").toggleClass("hidden");
+  };
 
-function changeNavToText() {
-  $("nav").html("<a class=\"nav-link\" href=\"#about\">about<a> <a class=\"nav-link\" href=\"#portfolio\">portfolio</a> <a class=\"nav-link\" href =\"#contact\">contact</a>");
-};
+  $(".nav-link").click(collapseHamburgerMenu);
 
-function changeNavToHamburger() {
-  $("nav").html("<div class=\"hamburger\" onclick=\"myFunction(this)\"><div class=\"bar1\"></div><div class=\"bar2\"></div><div class=\"bar3\"></div></div>");
-}
+  function changeNavToText() {
+    $("nav").html("<a class=\"nav-link\" href=\"#about\">about<a> " +
+                  "<a class=\"nav-link\" href=\"#portfolio\">portfolio</a> " +
+                  "<a class=\"nav-link\" href =\"#contact\">contact</a>");
+  };
 
-function myFunction3()
-{
-  if ($(window).width() > 700)
-      {
-          changeNavToText();
-          console.log($("#small-nav").is("hidden"));
-          if ($("#small-nav").attr("class")==="outer")
-          {
-              myFunction();
-          }
-      }
-  else changeNavToHamburger();
-};
+  function changeNavToHamburger() {
+    $("nav").html("<div id=\"hamburger\">" +
+                    "<div class=\"bar1\"></div>" +
+                    "<div class=\"bar2\"></div>" +
+                    "<div class=\"bar3\"></div>" +
+                  "</div>");
+    $("#hamburger").click(toggleHamburgerMenu);
+  }
 
-myFunction3;
-window.onresize =  myFunction3;
+  function setNavMode()
+  {
+    if ($(window).width() > 700)
+        {
+            changeNavToText();
+            collapseHamburgerMenu();
+        }
+    else changeNavToHamburger();
+  };
+
+  setNavMode();
+  window.onresize = setNavMode;
+});
