88b377297d7d630f7fa90d80e6235704a0484a20
Devin Cenatiempo <dcenatiempo@gmail.com>
2017-10-19 12:55:59 -0600

    Added random quote machine. Broke out header js/css

---
+++ js/home-site-header.js
@@ -0,0 +1,44 @@
+$(document).ready( () => {
+  function expandHamburgerMenu() {
+    $("#header-nav").addClass("change");
+    $("#small-nav").removeClass("hidden");
+  };
+  function collapseHamburgerMenu() {
+    $("#header-nav").removeClass("change");
+    $("#small-nav").addClass("hidden");
+  };
+  function toggleHamburgerMenu() {
+    $("#header-nav").toggleClass("change");
+    $("#small-nav").toggleClass("hidden");
+  };
+
+  $(".nav-link").click(collapseHamburgerMenu);
+
+  function changeNavToText() {
+    $("#header-nav").html("<a class=\"nav-link\" href=\"#about\">about<a> " +
+                  "<a class=\"nav-link\" href=\"#portfolio\">portfolio</a> " +
+                  "<a class=\"nav-link\" href =\"#contact\">contact</a>");
+  };
+
+  function changeNavToHamburger() {
+    $("#header-nav").html("<div id=\"hamburger\">" +
+                    "<div class=\"bar1\"></div>" +
+                    "<div class=\"bar2\"></div>" +
+                    "<div class=\"bar3\"></div>" +
+                  "</div>");
+    $("#hamburger").click(toggleHamburgerMenu);
+  }
+
+  function setNavMode()
+  {
+    if ($(window).width() > 700)
+        {
+            changeNavToText();
+            collapseHamburgerMenu();
+        }
+    else changeNavToHamburger();
+  };
+
+  setNavMode();
+  window.onresize = setNavMode;
+});
--- js/project-site-header.js
+++ js/project-site-header.js
@@ -1,27 +1,27 @@
 $(document).ready( () => {
   function expandHamburgerMenu() {
-    $("#nav").addClass("change");
+    $("#header-nav").addClass("change");
     $("#small-nav").removeClass("hidden");
   };
   function collapseHamburgerMenu() {
-    $("#nav").removeClass("change");
+    $("#header-nav").removeClass("change");
     $("#small-nav").addClass("hidden");
   };
   function toggleHamburgerMenu() {
-    $("#nav").toggleClass("change");
+    $("#header-nav").toggleClass("change");
     $("#small-nav").toggleClass("hidden");
   };
 
   $(".nav-link").click(collapseHamburgerMenu);
 
   function changeNavToText() {
-    $("nav").html("<a class=\"nav-link\" href=\"#about\">about<a> " +
-                  "<a class=\"nav-link\" href=\"#portfolio\">portfolio</a> " +
-                  "<a class=\"nav-link\" href =\"#contact\">contact</a>");
+    $("#header-nav").html("<a class=\"nav-link\" href=\".\\..\\index.html#about\">about<a> " +
+                  "<a class=\"nav-link\" href=\".\\..\\index.html#portfolio\">portfolio</a> " +
+                  "<a class=\"nav-link\" href =\".\\..\\index.html#contact\">contact</a>");
   };
 
   function changeNavToHamburger() {
-    $("nav").html("<div id=\"hamburger\">" +
+    $("#header-nav").html("<div id=\"hamburger\">" +
                     "<div class=\"bar1\"></div>" +
                     "<div class=\"bar2\"></div>" +
                     "<div class=\"bar3\"></div>" +
