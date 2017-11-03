eda81db94e9562160d36dbe17c43343fab449133
Devin Cenatiempo <dcenatiempo@gmail.com>
2017-11-03 15:34:37 -0600

    Uploaded v2

--- coin-calculator/js/coin-calc.js
+++ coin-calculator/js/coin-calc.js
@@ -1,30 +1,65 @@
-var usCoins = {
-  "1942-1945 Nickel": 0.056260,
-  "1892-1916 Barber Dime": 0.072336,
-  "1916-1945 Mercury Dime": 0.072336,
-  "1946-1964 Roosevelt Dime": 0.072336,
-  "1892-1916 Barber Quarter": 0.180848,
-  "1916-1930 Standing Liberty Quarter": 0.180848,
-  "1932-1964 Washington Quarter": 0.180848,
-  "1892-1915 Barber Half Dollar": 0.361695,
-  "1916-1947 Walking Liberty Half Dollar": 0.361695,
-  "1948-1963 Franklin Half Dollar": 0.361695,
-  "1964 Kennedy Half Dollar": 0.361695,
-  "1965-1970 Half Dollar (40% silver)": 0.147890,
-  "1878-1921 Morgan Dollar": 0.773450,
-  "1921-1935 Peace Dollar": 0.773450,
-  "1971-1976 Eisenhower Dollar (40% silver)": 0.316230,
-  "1986-2013 Silver Eagle (.999 Silver)": 0.999
-}
-var canadianCoins = {
-  "1920-1967 Dime": .06,
-  "1967-1968 Dime (50% silver)": .0375,
-  "1920-1967 Quarter": .15,
-  "1967-1968 Quarter (50% silver)": .09375,
-  "1920-1967 Half Dollar": .3,
-  "1935-1967 Dollar": .6
-}
-var currentPrice;
+var usSilverCoins = [
+  {
+    "name": "1942-1945 Nickel",
+    "metal content": 0.056260,
+    "qty": 0
+  },{
+    "name": "1892-1964 Dime",
+    "metal content": 0.072336,
+    "qty": 0
+  },{
+    "name": "1892-1964 Quarter",
+    "metal content": 0.180848,
+    "qty": 0
+  },{
+    "name": "1892-1964 Half Dollar",
+    "metal content": 0.361695,
+    "qty": 0
+  },{
+    "name": "1965-1970 Half Dollar",
+    "metal content": 0.147890,
+    "qty": 0
+  },{
+    "name": "1878-1935 Dollar",
+    "metal content": 0.773450,
+    "qty": 0
+  },{
+    "name": "1971-1976 Dollar",
+    "metal content": 0.316230,
+    "qty": 0
+  },{
+    "name": "1986-2013 Silver Eagle",
+    "metal content": 0.999,
+    "qty": 0
+  }
+];
+
+var usGoldCoins = [
+  {
+    "name": "1849 - 1889 $1.00",
+    "metal content": 0.048364,
+    "qty": 0
+  },{
+    "name": "1840 - 1929 $2.50",
+    "metal content": 0.120935,
+    "qty": 0
+  },{
+    "name": "1839 - 1929 $5.00",
+    "metal content": 0.241870,
+    "qty": 0
+  },{
+    "name": "1838 - 1933 $10.00",
+    "metal content": 0.483747,
+    "qty": 0
+  },{
+    "name": "1849 - 1933 $20.00",
+    "metal content": 0.967494,
+    "qty": 0
+  }
+];
+
+var currentSilverPrice = 16.5;
+var currentGoldPrice = 1268.70;
 var url ='http://www.coinflation.com/silver_coin_values.html';
 url = 'https://allorigins.us/get?method=raw&url=' + encodeURIComponent(url) +'&callback=?';
 
@@ -34,7 +69,7 @@ var request = new Request(url, {
 		'Content-Type': 'text/plain'
 	})
 });
-
+/*
 fetch(request)
   .then(function(response) {
     return response.text();
@@ -46,21 +81,22 @@ fetch(request)
     price = price.split("$")
     price = price[1];
     currentPrice = price;
-    h2Price = document.getElementById('current-price');
-    h2Price.innerHTML = '$'+currentPrice+'/oz';
-    displayCoins(usCoins, currentPrice)
+    //h2Price = document.getElementById('current-price');
+    //h2Price.innerHTML = '$'+currentPrice+'/oz';
+    //displayCoins(usSilverCoins, currentPrice)
   }).catch(function(err) {
       console.log(err);
-  });
+  });*/
  
-  function displayCoins (coin, pr){
-    var coinList = document.getElementById('coin-list');
-    for (prop in coin) {
-      console.log(prop +": $"+ (coin[prop]*pr));
-      var coinItem = document.createElement('h3');
-      coinItem.innerHTML = prop +": $"+ (coin[prop]*pr);
-      coinList.appendChild(coinItem);
-    }
+/* this needs to change to reflect new coin structure
+function displayCoins (coin, pr){
+  var coinList = document.getElementById('coin-list');
+  for (prop in coin) {
+    console.log(prop +": $"+ (coin[prop]*pr));
+    var coinItem = document.createElement('h3');
+    coinItem.innerHTML = prop +": $"+ (coin[prop]*pr);
+    coinList.appendChild(coinItem);
   }
+}*/
 
   //<b>Silver</b> $16.87/oz 
---
+++ coin-calculator/js/modal-popup.js
@@ -0,0 +1,186 @@
+document.getElementById('modal-btn')
+  .addEventListener('click', silverPopUp);
+
+function updateSilverPrice(e) {
+  let id = e.target.parentElement.id.split("silver")[1];
+  usSilverCoins[id].qty = e.target.value;
+  let priceElement = document.querySelectorAll('#silver' + id + ' .total-coin-price')[0];
+  let price = usSilverCoins[id].qty * usSilverCoins[id]['metal content']*currentSilverPrice;
+  price = price.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+  priceElement.innerText = price;
+  updateSilverTotal();
+}
+function updateGoldPrice(e) {
+  let id = e.target.parentElement.id.split("gold")[1];
+  usGoldCoins[id].qty = e.target.value;
+  let priceElement = document.querySelectorAll('#gold' + id + ' .total-coin-price')[0];
+  let price = usGoldCoins[id].qty * usGoldCoins[id]['metal content']*currentGoldPrice;
+  price = price.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+  priceElement.innerText = price;
+  updateGoldTotal();
+}
+
+function updateSilverTotal() {
+  let updated = usSilverCoins.reduce( (sum, e)=> {
+    return sum + (currentSilverPrice * e.qty * e['metal content'])
+  }, 0).toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+  document.querySelector('#modal-container h2').innerText = updated;
+}
+function updateGoldTotal() {
+  let updated = usGoldCoins.reduce( (sum, e)=> {
+    return sum + (currentGoldPrice * e.qty * e['metal content'])
+  }, 0).toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+  document.querySelector('#modal-container h2').innerText = updated;
+}
+
+function silverPopUp() {
+  var container = document.querySelector('#modal-container')
+  container.innerHTML = "";
+  container.style.visibility = 'visible';
+
+  var title = document.createElement('h1');
+  var titleText = document.createTextNode('Enter Silver Coins:');
+  title.appendChild(titleText);
+
+  container.appendChild(title);
+
+  usSilverCoins.forEach( (coin, i)=> {
+    let item = document.createElement('div');
+    item.setAttribute('class', 'coin-item');
+    item.setAttribute('id', "silver"+i)
+
+    let itemName = document.createElement('p');
+    itemName.setAttribute('class', 'coin-name');
+    let itemNameText = document.createTextNode(coin.name);
+    itemName.appendChild(itemNameText);
+
+    let itemPrice = document.createElement('p');
+    itemPrice.setAttribute('class', 'coin-price');
+    let iprice = currentSilverPrice * coin["metal content"];
+    iprice = iprice.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+    let itemPriceText = document.createTextNode( iprice );
+    itemPrice.appendChild(itemPriceText);
+
+    let itemQty = document.createElement('input');
+    itemQty.setAttribute('type', 'number');
+    itemQty.setAttribute('value', coin.qty);
+    itemQty.setAttribute('min', 0);
+    itemQty.addEventListener('input', updateSilverPrice)
+
+    let totalItemPrice = document.createElement('p');
+    totalItemPrice.setAttribute('class', 'total-coin-price');
+    let itotprice = currentSilverPrice * coin['metal content'] * itemQty.value;
+    itotprice = itotprice.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+    let totItemPriceText = document.createTextNode(itotprice);
+    totalItemPrice.appendChild(totItemPriceText);
+
+    item.appendChild(itemName);
+    item.appendChild(itemPrice);
+    item.appendChild(itemQty);
+    item.appendChild(totalItemPrice);
+
+    container.appendChild(item);
+  });
+  let h2Total = document.createElement('h2');
+  let h2Text = document.createTextNode('$0.00');
+  h2Total.appendChild(h2Text);
+
+  let nextBtn = document.createElement('button');
+  let btnText = document.createTextNode('Next');
+  nextBtn.appendChild(btnText);
+  nextBtn.addEventListener('click', goldPopUp);
+  
+  container.appendChild(h2Total);
+  container.appendChild(nextBtn);
+}
+
+function goldPopUp() {
+  var container = document.querySelector('#modal-container')
+  container.innerHTML = "";
+  container.style.visibility = 'visible';
+
+  var title = document.createElement('h1');
+  var titleText = document.createTextNode('Enter Gold Coins:');
+  title.appendChild(titleText);
+
+  container.appendChild(title);
+
+  usGoldCoins.forEach( (coin, i)=> {
+    let item = document.createElement('div');
+    item.setAttribute('class', 'coin-item');
+    item.setAttribute('id', "gold"+i)
+
+    let itemName = document.createElement('p');
+    itemName.setAttribute('class', 'coin-name');
+    let itemNameText = document.createTextNode(coin.name);
+    itemName.appendChild(itemNameText);
+
+    let itemPrice = document.createElement('p');
+    itemPrice.setAttribute('class', 'coin-price');
+    let iprice = currentGoldPrice * coin["metal content"];
+    iprice = iprice.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+    let itemPriceText = document.createTextNode( iprice );
+    itemPrice.appendChild(itemPriceText);
+
+    let itemQty = document.createElement('input');
+    itemQty.setAttribute('type', 'number');
+    itemQty.setAttribute('value', coin.qty);
+    itemQty.setAttribute('min', 0);
+    itemQty.addEventListener('input', updateGoldPrice)
+
+    let totalItemPrice = document.createElement('p');
+    totalItemPrice.setAttribute('class', 'total-coin-price');
+    let itotprice = currentGoldPrice * coin['metal content'] * itemQty.value;
+    itotprice = itotprice.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+    let totItemPriceText = document.createTextNode(itotprice);
+    totalItemPrice.appendChild(totItemPriceText);
+
+    item.appendChild(itemName);
+    item.appendChild(itemPrice);
+    item.appendChild(itemQty);
+    item.appendChild(totalItemPrice);
+
+    container.appendChild(item);
+  });
+  let h2Total = document.createElement('h2');
+  let h2Text = document.createTextNode('$0.00');
+  h2Total.appendChild(h2Text);
+
+  let nextBtn = document.createElement('button');
+  let btnText = document.createTextNode('Next');
+  nextBtn.appendChild(btnText);
+  nextBtn.addEventListener('click', otherPopup);
+  
+  container.appendChild(h2Total);
+  container.appendChild(nextBtn);
+}
+
+function otherPopup() {
+  var container = document.querySelector('#modal-container')
+  container.innerHTML = "";
+  container.style.visibility = 'hidden';
+}
