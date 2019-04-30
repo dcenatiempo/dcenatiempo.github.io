d763434506b204de53b1eceb064f12924e6c921a
Devin Cenatiempo <dcenatiempo@insiderealestate.com>
2019-04-29 20:18:16 -0600

    mvp madlab price-point

---
+++ madlab/price-point/precache-manifest.e374889a85365b5df34961816d98e0e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "13614b43cb867ddef202",
+    "url": "/js/chunk-vendors.3acb87d5.js"
+  },
+  {
+    "revision": "b28b3b46e3fdf713a84f",
+    "url": "/js/app.ad1d783d.js"
+  },
+  {
+    "revision": "467efaa3cfab639e9735bb3283c55104",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f907ffabde10b99b33363f7b10d8068c",
+    "url": "/img/moosecock_rooster_final.f907ffab.png"
+  },
+  {
+    "revision": "d5358702badac6aa75ed44a3ac31fb81",
+    "url": "/img/logo.d5358702.png"
+  },
+  {
+    "revision": "b28b3b46e3fdf713a84f",
+    "url": "/css/app.b2669f21.css"
+  }
+];
